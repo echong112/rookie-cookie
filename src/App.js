a34167d3ebe657b87a73fef2b0bca4026ce74eba
Enrique <echong112@gmail.com>
2019-12-02 15:50:03 -0500

    processImageKey(content) to parse inconsistent mock API response.

--- src/App.js
+++ src/App.js
@@ -21,9 +21,30 @@ const App = () => {
 
 export default App
 
+// NOTE: Obviously, the easy / cheat answer is to update the classes.js
+// Assuming the data object has something that has something
+// along the lines of 'featuredImage' or 'featureImage', it will parse the keys and find
+// the correct object param to populate the imageUrl
+
+const processImageKey = (content) => {
+  let keys = Object.keys(content);
+  let imageUrl = '';
+  keys.forEach((key, i) => {
+    let featureRegEx = /^featureImage/;
+    let featuredRegEx = /^featuredImage/;
+    if (featureRegEx.test(key) || featuredRegEx.test(key)) {
+      imageUrl = content[key];
+    }
+  });
+  return imageUrl;
+}
+
 const ClassCard = ({content}) => (
   <ClassCardWrapper>
-    <img height="50%" width="100%" src={content.featureImage} alt='' />
+    <img height="50%" width="100%"
+      src={processImageKey(content)}
+      alt=''
+    />
     <h4>{content.title}</h4>
     <h5>{content.instructor}</h5>
     <h5>{content.description}</h5>
