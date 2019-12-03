09c4aa8293e621cc6d71e5c89f716a084a4fca4b
Enrique <echong112@gmail.com>
2019-12-03 11:47:20 -0500

    class id update. remove and cancel class

--- src/App.js
+++ src/App.js
@@ -7,19 +7,32 @@ import { processImageKey, truncateDesc } from './utils'
 
 const App = () => {
   const [classes, setClasses] = useState([])
-
   useEffect(() => {
     setClasses(classList)
   }, [])
+
+  const deleteClass = (id) => {
+    setClasses(classes.filter(curClass => curClass.id !== id));
+  }
+
+  const cancelClass = (id) => {
+    console.log(id);
+  }
 
   return (
     <Wrapper>
       <NavBar />
       <h1>Welcome to RookieCookie!</h1>
       <CardsWrapper>
-        {classes.map((klass, i) => {
-          return <ClassCard key={i} index={i} content={klass}/>
-        })}
+        {classes.map((klass, i) => (
+            <ClassCard
+              key={i}
+              content={klass}
+              onCancel={cancelClass.bind(this, klass.id)}
+              onDelete={deleteClass.bind(this, klass.id)}
+            />
+          )
+        )}
       </CardsWrapper>
       <AddClass onSubmit={newClass => setClasses([...classes, newClass]) }/>
     </Wrapper>
@@ -28,22 +41,45 @@ const App = () => {
 
 export default App
 
-const ClassCard = ({index, content}) => (
-  <ClassCardWrapper>
-    <ClassCardImage
-      style ={ { backgroundImage: `url(${processImageKey(content)})` } }
-      src={processImageKey(content)}
-    />
-    <h4>{content.title}</h4>
-    <h5>{content.instructor}</h5>
-    <h5>{truncateDesc(content.description)}</h5>
-    <ClassCardDuration>{content.duration} min</ClassCardDuration>
-    <ClassButtonContainer>
-      <button>Cancel</button>
-      <button>Delete</button>
-    </ClassButtonContainer>
-  </ClassCardWrapper>
-)
+const ClassCard = ({index, content, onDelete, onCancel}) => {
+  const [currClass, setCurrClass] = useState(content);
+
+  const cancelClass = () => {
+    let tempClass = Object.assign({}, content);
+    tempClass.isCancelled = true;
+    setCurrClass(tempClass);
+  }
+
+  return (
+    <ClassCardWrapper>
+      <ClassCardImage
+        style ={ { backgroundImage: `url(${processImageKey(currClass)})` } }
+        src={processImageKey(currClass)}
+      />
+      {currClass.isCancelled && (
+        <CancelledBadge>Cancelled</CancelledBadge>
+      )}
+      <h4>{currClass.title}</h4>
+      <h5>{currClass.instructor}</h5>
+      <h5>{truncateDesc(currClass.description)}</h5>
+      <ClassCardDuration>{currClass.duration} min</ClassCardDuration>
+      <ClassButtonContainer>
+        <button onClick={onDelete}>Delete</button>
+        <button onClick={cancelClass}>Cancel</button>
+      </ClassButtonContainer>
+    </ClassCardWrapper>
+  )
+}
+
+const CancelledBadge = styled.div`
+  position: absolute;
+  width: 99%;
+  top: 0;
+  left: 0;
+  padding: 15px 0;
+  font-weight: bold;
+  color: red;
+`
 
 const Wrapper = styled.div `
   text-align: center;
